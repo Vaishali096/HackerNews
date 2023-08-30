7bf49f84062d116f1e1924824fe37de4aae9c94f
Ehsan Shahdoust <ehsan.shahdoust@gmail.com>
2023-08-30 15:24:31 +0200

    Added components

--- React-HackerProject/src/App.jsx
+++ React-HackerProject/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-
+      <div>Ehsan searcbar</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
