95f98df67a01823c03aede237839e7b3f7f50c55
Ehsan Shahdoust <ehsan.shahdoust@gmail.com>
2023-08-31 14:12:11 +0200

    Updated searchbar and used url search, added

--- React-HackerProject/src/components/SearchResult.jsx
+++ React-HackerProject/src/components/SearchResult.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
+// Showing the result with specific key and value
 const SearchResult = ({ results }) => {
   return (
     <div>
       {results.map((res) => (
-        <div key={res.author}>
+        <div key={res.title}>
           <h2>{res.title}</h2>
+          <p>
+            {res.points}by
+            {res.author}
+          </p>
         </div>
       ))}
     </div>
--- React-HackerProject/src/components/Searchbar.jsx
+++ React-HackerProject/src/components/Searchbar.jsx
@@ -1,44 +1,40 @@
 import { useEffect, useState } from "react";
 
+// Search the specific topic otherwise shows all
 function SearchBar({ setResults }) {
-  // const [data, setData] = useState([]);
   const [value, setValue] = useState();
-  // const [result, setResult] = useState();
 
   async function searchEndpoint(value) {
     const fetchSearch = await fetch(
-      "http://hn.algolia.com/api/v1/search?query=..."
+      `http://hn.algolia.com/api/v1/search?query=${value}`
     );
     const res = await fetchSearch.json();
-    const filterdRes = res.hits.filter((user) => {
-      return value && user.title.toLowerCase().includes(value);
-    });
-    setResults(filterdRes);
+    setResults(res.hits);
   }
 
+  // Handle the changes and set value here
   const handleChange = (value) => {
     setValue(value);
     searchEndpoint(value);
   };
 
+  //Used effect to show all items at first
+  useEffect(() => {
+    searchEndpoint(value);
+  }, []);
+
   return (
     <>
-      <input
-        type="text"
-        placeholder="Type to search..."
-        value={value}
-        onChange={(e) => {
-          handleChange(e.target.value);
-        }}
-      />
-
-      <button
-        onClick={() => {
-          console.log("value", value);
-        }}
-      >
-        Search
-      </button>
+      <from>
+        <input
+          type="text"
+          placeholder="Type to search..."
+          value={value}
+          onChange={(e) => {
+            handleChange(e.target.value);
+          }}
+        />
+      </from>
     </>
   );
 }
