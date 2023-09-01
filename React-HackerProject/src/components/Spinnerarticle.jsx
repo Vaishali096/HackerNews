498b5cdefa8b7bf6881ea260080756a0ef39e9b4
Vaishali096 <vaishalivenkateshwaran@gmail.com>
2023-09-01 12:31:41 +0200

    merged pagination

--- React-HackerProject/src/components/Spinnerarticle.jsx
+++
@@ -1,9 +0,0 @@
-export default function Spinnerarticle({ post }) {
-    console.log(post);
-    return (
-      <div>
-        <h1>{post.title}</h1> |<p>by: {post.author}</p>
-        <p>points: {post.points}</p>
-      </div>
-    );
-  }
